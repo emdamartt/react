6c727099e08e3b17d69528186f0f39cdf0762568
Emanuel Martinez <emanuel.mg86@gmail.com>
2024-03-17 07:53:58 -0300

    se modifico el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,29 +2,29 @@ const Footer = () => {
     
     return (
         <>
-            <footer className="mt-20 mb-10 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 shadow-2xl">
-                <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4">
-                <div className="sm:col-span-2">
-                    <a href="/" aria-label="Go home" title="Company" className="inline-flex items-center">
-                        <img src="./src/assets/img/corpCap.svg" alt="logo company" className="w-20" fill="none" />
-                    </a>
-                    <div className="mt-6 lg:max-w-sm">
-                        <p className="text-sm text-gray-800">
-                            Capsule Corporation es una empresa apasionada que se especializa en la venta de muñecos de anime inspirados en la icónica serie Dragon Ball. 
-                        </p>
-                        <p className="mt-4 text-sm text-gray-800">
-                        Nos dedicamos a ofrecer a los fans de todas las edades la oportunidad de coleccionar y disfrutar de figuras de alta calidad que capturan la esencia y la emoción del universo Dragon Ball.
-                        </p>
+            <footer className="mt-6 mb-10 mr-4 ml-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xxl md:px-24 lg:px-8 shadow-2xl">
+                <div className="grid sm:grid-cols-2 lg:grid-cols-4">
+                    <div className="sm:col-span-2 mt-6">
+                        <a href="/" aria-label="Go home" title="Company" className="inline-flex items-center">
+                            <img src="./src/assets/img/corpCap.svg" alt="logo company" className="w-10" fill="none" />
+                        </a>
+                        <div className="mt-4 mb-4 lg:max-w-sm">
+                            <p className="text-sm text-gray-800">
+                                Capsule Corporation es una empresa apasionada que se especializa en la venta de muñecos de anime inspirados en la icónica serie Dragon Ball. 
+                            </p>
+                            <p className="mt-4 text-sm text-gray-800">
+                            Nos dedicamos a ofrecer a los fans de todas las edades la oportunidad de coleccionar y disfrutar de figuras de alta calidad que capturan la esencia y la emoción del universo Dragon Ball.
+                            </p>
+                        </div>
                     </div>
-                    </div>
-                    <div className="space-y-2 text-sm">
-                        <p className="text-xl tracking-wide text-yellow-300 font-saiyan">Contacts</p>
+                    <div className="space-y-2 text-sm mt-1">
+                        <p className="text-xl tracking-wide text-yellow-300 font-saiyan mb-11">Contacts</p>
                     <div className="flex">
                         <p className="mr-1 text-gray-800 font-saiyan ">Phone:</p>
                         <a href="tel:+549-(11)-6212-8806" aria-label="Our phone" title="Our phone" className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800">+549-(11)-6212-8806</a>
                         </div>
                         <div className="flex">
-                            <p className="mr-1 text-gray-800 font-saiyan ">Email:</p>
+                            <p className="mr-1 text-gray-800 font-saiyan">Email:</p>
                             <a href="mailto:info@lorem.mail" aria-label="Our email" title="Our email" className="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800">MartDesing@gmail.com</a>
                         </div>
                         <div className="flex">
@@ -33,9 +33,9 @@ const Footer = () => {
                             254 Cabildo av, Bs As, Argentina.</a>
                         </div>
                     </div>
-                    <div>
-                        <span className="text-xl tracking-wide text-red-600 font-saiyan">Social</span>
-                        <div className="flex p-2 mt-1">
+                    <div className="space-y-2 text-sm mt-4">
+                        <p className="text-xl tracking-wide text-red-600 font-saiyan mb-6">Social</p>
+                        <div className="flex">
                             <a href="/" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400 p-1">
                             <svg x="0px" y="0px" height="24" viewBox="0 0 48 48">
                                 <path fill="#0078d4" d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5	V37z"></path><path d="M30,37V26.901c0-1.689-0.819-2.698-2.192-2.698c-0.815,0-1.414,0.459-1.779,1.364	c-0.017,0.064-0.041,0.325-0.031,1.114L26,37h-7V18h7v1.061C27.022,18.356,28.275,18,29.738,18c4.547,0,7.261,3.093,7.261,8.274	L37,37H30z M11,37V18h3.457C12.454,18,11,16.528,11,14.499C11,12.472,12.478,11,14.514,11c2.012,0,3.445,1.431,3.486,3.479	C18,16.523,16.521,18,14.485,18H18v19H11z" opacity=".05"></path><path d="M30.5,36.5v-9.599c0-1.973-1.031-3.198-2.692-3.198c-1.295,0-1.935,0.912-2.243,1.677	c-0.082,0.199-0.071,0.989-0.067,1.326L25.5,36.5h-6v-18h6v1.638c0.795-0.823,2.075-1.638,4.238-1.638	c4.233,0,6.761,2.906,6.761,7.774L36.5,36.5H30.5z M11.5,36.5v-18h6v18H11.5z M14.457,17.5c-1.713,0-2.957-1.262-2.957-3.001	c0-1.738,1.268-2.999,3.014-2.999c1.724,0,2.951,1.229,2.986,2.989c0,1.749-1.268,3.011-3.015,3.011H14.457z" opacity=".07"></path><path fill="#fff" d="M12,19h5v17h-5V19z M14.485,17h-0.028C12.965,17,12,15.888,12,14.499C12,13.08,12.995,12,14.514,12	c1.521,0,2.458,1.08,2.486,2.499C17,15.887,16.035,17,14.485,17z M36,36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698	c-1.501,0-2.313,1.012-2.707,1.99C24.957,25.543,25,26.511,25,27v9h-5V19h5v2.616C25.721,20.5,26.85,19,29.738,19	c3.578,0,6.261,2.25,6.261,7.274L36,36L36,36z"></path>
@@ -52,7 +52,7 @@ const Footer = () => {
                             </svg>
                             </a>
                         </div>
-                            <p>Te invitamos a conocer nuestros sitios en donde podras tener contacto personalizado con nosotros.</p>
+                            <p className="text-sm text-gray-800">Te invitamos a conocer nuestros sitios en donde podras tener contacto personalizado con nosotros.</p>
                     </div>
                 </div>
                 <div className="flex flex-col-reverse justify-between pt-5 pb-10 border-t lg:flex-row">
